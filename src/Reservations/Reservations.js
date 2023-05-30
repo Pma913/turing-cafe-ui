1ee7cd01d4f2e6cca02ae404d712dbb9069a2c53
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-05-30 12:14:39 -0400

    Add basic styling to App ui including but not limited to sizing, flex display and color matching

--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from '../Card/Card'
+import './Reservations.css'
 
 const Reservations = ({ reservations }) => {
   const resList = reservations.map(res => <Card key={res.id} reso={res} />)
