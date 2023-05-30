1bdd7fcc9b346a3752863de92d485e5ab7c058a5
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-05-30 13:23:24 -0400

    Add functionality to make reservation form to fill in fields and submit form to see the new reservation populate

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Card.css'
 
-const Card = () => {
+const Card = ({ reso}) => {
   return (
     <div className='card'>
-      <h2 className="name">Name</h2>
-      <p className="date">Date</p>
-      <p className="time">time</p>
-      <p className="guests">Number of guests:</p>
+      <h2 className="name">{reso.name}</h2>
+      <p className="date">{reso.date}</p>
+      <p className="time">{reso.time} pm</p>
+      <p className="guests">Number of guests: {reso.number}</p>
       <button className="cancel-btn">Cancel</button>
     </div>
   )
--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -3,7 +3,7 @@ import Card from '../Card/Card'
 import './Reservations.css'
 
 const Reservations = ({ reservations }) => {
-  const resList = reservations.map(res => <Card key={res.id} reso={res} />)
+  const resList = reservations.map(res => <Card key={res.id} reso={res}/>)
   return (
     <div className="res">
       {resList}
